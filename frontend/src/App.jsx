d07704c1f8461adcf7b15e72ac3d6049a3149f4a
Nikita Pavlov <pavlovkeith@gmail.com>
2024-11-27 16:13:10 +0300

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,86 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  Navigate,
+} from 'react-router-dom';
+import { Button, Navbar } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+// import { toast, ToastContainer } from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+// import axios from 'axios';
+
+import NotFoundPage from './pages/NotFoundPage';
+import LoginPage from './pages/LoginPage';
+import HomePage from './pages/HomePage';
+// import Add from './components/modal/Add';
+
+import { logOut } from './store/slices/authSlice';
+
+// import { actions } from './store/slices/channelsSlice';
+
+const PrivateRoute = ({ children }) => {
+  const { loggedIn } = useSelector((state) => state.auth);
+  return (
+    loggedIn ? children : <Navigate to="/login" />
+  );
+};
+
+const AuthButton = () => {
+  const dispatch = useDispatch();
+  const { loggedIn } = useSelector((state) => state.auth);
+  return (
+    loggedIn ? <Button onClick={() => dispatch(logOut())}>Выйти</Button> : null
+  );
+};
+
+// const getSuccessToast = (toastMessage = null) => {
+//   if (toastMessage) {
+//     toast.success(toastMessage);
+//   }
+// };
+
+const App = () => {
+  // const getSuccessToast = (toastMessage = null) => {
+  //   // console.log('fff');
+  //   // console.log(toastMessage);
+  //   if (toastMessage) {
+  //     console.log(toastMessage);
+  //     toast.success(toastMessage);
+  //   }
+  // };
+
+  console.log('App');
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <Router>
+      <Navbar bg="white" expand="lg" className="shadow-sm">
+        <div className="container">
+          <Navbar.Brand as={Link} to="/">Hexlet Chat</Navbar.Brand>
+          <AuthButton />
+        </div>
+      </Navbar>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        {/* <Route path="/signup" element={<Signup />} /> */}
+        <Route path="*" element={<NotFoundPage />} />
+        <Route
+          path="/"
+          element={(
+            <PrivateRoute>
+              <HomePage />
+              {/* <ToastContainer /> */}
+            </PrivateRoute>
+          )}
+        />
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
