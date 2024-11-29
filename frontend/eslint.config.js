20e7cc9fd547b96062302142da9a952a66a2156a
Nikita Pavlov <pavlovkeith@gmail.com>
2024-11-29 16:49:09 +0300

    update

---
+++ frontend/eslint.config.js
@@ -0,0 +1,19 @@
+export default [
+  {
+    files: ['**/*.{js,jsx}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    rules: {
+      'eol-last': ['error', 'always'],
+      'no-console': 'off',
+      'react/react-in-jsx-scope': 'off',
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+];
